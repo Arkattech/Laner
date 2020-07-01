29ae10c03c0ad60ed94b65fa38d85f807b4b90cd
Elias Cazessus <cazessus.elias@gmail.com>
2020-06-30 21:10:55 -0700

    fixed mobile notice dismissal

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
+  if (window.innerWidth < 1366){
+    document.querySelector('.inner-cv').innerHTML = '<h4>Due to the current Coronavirus pandemic</h4> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. <a href="#" class="x"><h6 style="text-decoration: underline; margin: 5px;">dismiss</h6></a>'
+  } else {
+
+  }
   let covid = document.querySelector('.covid');
   covid.style.display = 'inline-block';
 
