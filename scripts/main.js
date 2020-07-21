b8f51b51311d36b6f47cd123ab40acb60902baa3
Elias-Cz <cazessus.elias@gmail.com>
2020-07-21 14:49:53 -0700

    7-21 changes

--- scripts/main.js
+++ scripts/main.js
@@ -5,14 +5,18 @@ document.addEventListener('DOMContentLoaded', () => {
 
   }
   let covid = document.querySelector('.covid');
+  let blur = document.querySelector('.blur')
   covid.style.display = 'inline-block';
+  blur.style.filter = 'blur(2px)'
 
   document.querySelector('.x').onclick = () => {
     covid.style.display = 'none';
+    blur.style.filter = 'none'
   }
 
   document.onscroll = () => {
     covid.style.display = 'none';
+    blur.style.filter = 'none'
   }
 });
 
@@ -30,9 +34,13 @@ function navScroll() {
 
 document.querySelector('.place').onclick = () => {
   let order = document.querySelector('.order');
+  let blur = document.querySelector('.blur');
   order.style.display = 'inline-block';
+  blur.style.filter = 'blur(2px)';
   document.querySelector('#x').onclick = () => {
     order.style.display = 'none';
+    blur.style.filter = 'none';
+
   }
 }
 
@@ -41,10 +49,14 @@ function addField(){
   var input = document.createElement('input');
   input.type = "text";
   input.name = 'lineitem'
+  input.setAttribute('class', 'line-item')
+  input.placeholder = 'Item'
+  input.style = 'max-width: 400px;'
   var num = document.createElement('input');
   num.type="number"
   num.name = 'lineq'
   num.style = 'width: 40px;'
+  num.setAttribute('class', 'line-item')
   container.append(input);
   container.append(num);
   container.append(document.createElement('br'));
