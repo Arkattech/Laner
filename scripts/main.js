0855560bb75a91815c0107f87ce2de9606a1da83
Elias-Cz <cazessus.elias@gmail.com>
2020-08-03 14:20:13 -0700

    ajax int

--- scripts/main.js
+++ scripts/main.js
@@ -65,3 +65,15 @@ function addField(){
   container.append(num);
   container.append(document.createElement('br'));
 }
+
+function loadDoc() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("dyn").innerHTML =
+      this.responseText;
+    }
+  };
+  xhttp.open("GET", "corp.txt", true);
+  xhttp.send();
+}
