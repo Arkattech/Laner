a1ad3e2037fee4647a5ab6ac43f492ab36494b07
Elias Cazessus <cazessus.elias@gmail.com>
2020-09-11 11:28:52 -0700

    covid change

--- scripts/main.js
+++ scripts/main.js
@@ -136,7 +136,7 @@ function loadDoc2() {
   };
   let map = document.querySelector(".map-link")
   map.textContent = "Hide";
-  
+
   map.onclick = () => {
     loadDoc()
     map.textContent = "Map"
@@ -144,6 +144,18 @@ function loadDoc2() {
   }
   document.querySelector("#majax").scrollIntoView(true)
   xhttp.open("GET", "map.txt", true);
+  xhttp.send();
+}
+
+function loadDoc3() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("covp-ajax").innerHTML =
+      this.responseText;
+    }
+  };
+  xhttp.open("GET", "covid.txt", true);
   xhttp.send();
 }
 
